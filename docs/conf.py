53ed2bd677f0df89fd28213db8d1493923cab97c
cave beat <cave@cavebeat.org>
2016-09-21 23:52:19 +0200

    license+title

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'RetroShare'
+copyright = u'CC-BY-SA 4.0'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
